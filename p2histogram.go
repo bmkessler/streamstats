4a247a1a7d5eb94e041d0cac2ada695341e7024e
Brian Kessler <brian.m.kessler@gmail.com>
2017-02-09 22:31:24 -0800

    P2Histogram completed testing and fixed bugs in linear interpolation

--- p2histogram.go
+++ p2histogram.go
@@ -6,16 +6,16 @@ package streamstats
 // Histograms Without Storing Observations" by RAJ JAIN and IIMRICH CHLAMTAC
 // Communications of the ACM Volume 28 Issue 10, Oct. 1985 Pages 1076-1085
 type P2Histogram struct {
-	b int       // the number of bins to be tracked
+	b uint64    // the number of bins to be tracked
 	n []uint64  // the actual counts for each marker
 	q []float64 // the value of each marker, i.e. the estimated quantile
 }
 
 // NewP2Histogram intializes the data structure to track b bins
-func NewP2Histogram(b int) P2Histogram {
+func NewP2Histogram(b uint64) P2Histogram {
 	n := make([]uint64, b+1, b+1)
 	q := make([]float64, b+1, b+1)
-	for i := 0; i < b; i++ {
+	for i := uint64(0); i < b; i++ {
 		n[i] = uint64(i) + 1
 	}
 	return P2Histogram{
@@ -50,7 +50,7 @@ func (h *P2Histogram) Add(x float64) {
 			h.q[h.b] = x // new maximum
 			k = uint64(h.b) - 1
 		} else { // check which bin the measurement falls into
-			for i := 1; i <= h.b; i++ {
+			for i := uint64(1); i <= h.b; i++ {
 				if x < h.q[i] {
 					k = uint64(i - 1)
 					break
@@ -62,7 +62,7 @@ func (h *P2Histogram) Add(x float64) {
 			h.n[i]++
 		}
 		// adjust heights of internal markers if necessary
-		for i := 1; i < h.b; i++ {
+		for i := uint64(1); i < h.b; i++ {
 			np := 1.0 + float64(i)*(float64(h.n[h.b])-1.0)/float64(h.b)
 			d := np - float64(h.n[i]) // the difference from the target
 			if (d >= 1.0 && h.n[i]+1 < h.n[i+1]) || (d <= -1.0 && h.n[i-1]+1 < h.n[i]) {
@@ -80,7 +80,7 @@ func (h *P2Histogram) Add(x float64) {
 				if h.q[i-1] < qp && qp < h.q[i+1] {
 					h.q[i] = qp
 				} else { // use linear formula if degree 2 formula would result in out of order markers
-					ip := i + int(d)
+					ip := int(i) + int(d)
 					h.q[i] += d * (h.q[ip] - h.q[i]) / (float64(h.n[ip]) - fN)
 				}
 				if d > 0 { // increment the counter for the bin after adjustments were made
@@ -107,10 +107,14 @@ type CumulativeDensity struct {
 
 // Histogram returns the histogram of observations seen so far
 func (h *P2Histogram) Histogram() []CumulativeDensity {
-
-	cdf := make([]CumulativeDensity, h.b+1, h.b+1)
-	fN := float64(h.N())
-	for i := 0; i < h.b+1; i++ {
+	N := h.N()
+	L := h.b + 1
+	if N < L {
+		L = N
+	}
+	cdf := make([]CumulativeDensity, L, L)
+	fN := float64(N)
+	for i := uint64(0); i < L; i++ {
 		cdf[i] = CumulativeDensity{X: h.q[i], P: float64(h.n[i]) / fN}
 	}
 	return cdf
@@ -124,9 +128,9 @@ func (h *P2Histogram) Min() float64 {
 
 // Max returns the maximum of observations seen so far
 func (h *P2Histogram) Max() float64 {
-
-	if h.n[h.b] < uint64(h.b)+1 && 0 < h.n[h.b] {
-		return h.q[h.n[h.b]-1]
+	N := h.n[h.b]
+	if N < uint64(h.b)+1 && 0 < N {
+		return h.q[N-1]
 	}
 	return h.q[h.b]
 }
@@ -134,39 +138,39 @@ func (h *P2Histogram) Max() float64 {
 // Quantile returns the linear approximation to the given quantile based on the histogram data
 func (h *P2Histogram) Quantile(p float64) float64 {
 
-	var q float64
+	// check the bounds for percentage between 0 and 1
 	if p <= 0.0 {
-		q = h.Min()
+		return h.Min()
 	} else if 1.0 <= p {
-		q = h.Max()
+		return h.Max()
 	}
-	N := uint64(h.b)
-	if h.n[h.b] < uint64(h.b)+1 && 0 < h.n[h.b] {
-		N = h.n[h.b] // there aren't enough values yet
+	CDF := h.Histogram()
+	var i uint64 // find which bin the given percentage is in
+	for i = 0; i < h.b; i++ {
+		if CDF[i].P <= p && p < CDF[i+1].P {
+			break
+		}
 	}
-	fN := float64(h.N())
-	for i := uint64(0); i < N && float64(h.n[i])/fN < p; i++ {
-		q = h.q[i] + p*fN*(h.q[i+1]-h.q[i])/(float64(h.n[i+1])-float64(h.n[i]))
-	}
-	return q
+	// linear interpolation
+	return CDF[i].X + (CDF[i+1].X-CDF[i].X)*(p-CDF[i].P)/(CDF[i+1].P-CDF[i].P)
 }
 
 // CDF returns the linear approximation to the CDF at x based on the histogram data
 func (h *P2Histogram) CDF(x float64) float64 {
 
-	var q float64
+	// check that x is between the Min and the Max
 	if x < h.Min() {
-		q = 0.0
+		return 0.0
 	} else if h.Max() <= x {
-		q = 1.0
+		return 1.0
 	}
-	N := uint64(h.b)
-	if h.n[h.b] < uint64(h.b)+1 && 0 < h.n[h.b] {
-		N = h.n[h.b] // there aren't enough values yet
+	CDF := h.Histogram()
+	var i uint64 // find which bin the given x is in
+	for i = 0; i < h.b; i++ {
+		if CDF[i].X <= x && x < CDF[i+1].X {
+			break
+		}
 	}
-	fN := float64(h.N())
-	for i := uint64(0); i < N && h.q[i] < x; i++ {
-		q = float64(h.n[i])/fN + x*(float64(h.n[i+1])-float64(h.n[i]))/(fN*(h.q[i+1]-h.q[i]))
-	}
-	return q
+	// linear interpolation
+	return CDF[i].P + (CDF[i+1].P-CDF[i].P)*(x-CDF[i].X)/(CDF[i+1].X-CDF[i].X)
 }
--- p2histogram_test.go
+++ p2histogram_test.go
@@ -2,6 +2,8 @@ package streamstats
 
 import (
 	"math"
+	"math/rand"
+	"reflect"
 	"testing"
 )
 
@@ -23,7 +25,7 @@ func TestNewP2Histogram(t *testing.T) {
 	if h.b != initialP2H4.b {
 		t.Errorf("Expected b to be %v, got %v", initialP2H4.b, h.b)
 	}
-	if len(h.n) != h.b+1 {
+	if uint64(len(h.n)) != h.b+1 {
 		t.Errorf("Expected len(n)==%v, got len(n)=%v", h.b+1, len(h.n))
 	}
 	for j := 0; j < len(h.n); j++ {
@@ -42,7 +44,7 @@ func TestNewP2Histogram(t *testing.T) {
 	if h.b != initialP2H10.b {
 		t.Errorf("Expected b to be %v, got %v", initialP2H10.b, h.b)
 	}
-	if len(h.n) != h.b+1 {
+	if uint64(len(h.n)) != h.b+1 {
 		t.Errorf("Expected len(n)==%v, got len(n)=%v", h.b+1, len(h.n))
 	}
 	for j := 0; j < len(h.n); j++ {
@@ -62,7 +64,7 @@ var histogramSmallNTestData = []float64{4, 6, 5, 7, 3, 1, 2}
 var histogramSmallNExpectedData = []float64{1, 2, 3, 4, 5, 6, 7}
 
 func TestP2HistorgramSmallN(t *testing.T) {
-	h := NewP2Histogram(len(histogramSmallNTestData) - 1)
+	h := NewP2Histogram(uint64(len(histogramSmallNTestData) - 1))
 	for _, x := range histogramSmallNTestData {
 		h.Add(x)
 	}
@@ -73,6 +75,34 @@ func TestP2HistorgramSmallN(t *testing.T) {
 		if h.q[i] != x {
 			t.Errorf("Expected q[%v]=%v, got q[%v]=%v", i, x, i, h.q[i])
 		}
+	}
+	min := 1.0
+	max := 3.0
+	Npoints := uint64(2)
+	Nbins := uint64(20)
+	q := NewP2Histogram(Nbins)
+	q.Add(min)
+	q.Add(max)
+	expectedHistogram := []CumulativeDensity{{X: min, P: 1.0 / float64(Npoints)}, {X: max, P: 2.0 / float64(Npoints)}}
+	if Npoints != q.N() {
+		t.Errorf("Expected N: %v got %v", Npoints, q.N())
+	}
+	if min != q.Min() {
+		t.Errorf("Expected Min: %v got %v", min, q.Min())
+	}
+	if max != q.Max() {
+		t.Errorf("Expected Max: %v got %v", max, q.Max())
+	}
+	if reflect.DeepEqual(expectedHistogram, q.Histogram()) != true {
+		t.Errorf("Expected Histogram: %v got %v", expectedHistogram, q.Histogram())
+	}
+	p := 0.75
+	expectedQ := 2.0
+	if p != q.CDF(expectedQ) {
+		t.Logf("Expected p: %v got %v", p, q.Quantile(expectedQ))
+	}
+	if expectedQ != q.Quantile(p) {
+		t.Logf("Expected x: %v got %v", expectedQ, q.Quantile(p))
 	}
 }
 
@@ -100,6 +130,58 @@ func TestP2DataPointsHistogram(t *testing.T) {
 		}
 		if hist.Max() != q.Max() {
 			t.Errorf("Expected Max to be %d got %d", q.Max(), hist.Max())
+		}
+	}
+}
+
+func TestP2HistogramExponentialDist(t *testing.T) {
+	rand.Seed(42) // for deterministic testing
+	N := 100000
+	Nbins := uint64(20)
+	eps := 0.03 // expect errors less than 3% for all quantiles
+	lambdas := []float64{1.0, 2.0, 0.5}
+	ps := []float64{0.05, 0.10, 0.17, 0.25, 0.50, 0.65, 0.78, 0.95}
+	for _, lambda := range lambdas {
+		q := NewP2Histogram(Nbins)
+		for i := 0; i < N; i++ {
+			q.Add(exponentialRandomVariable(lambda))
+		}
+		for i, cd := range q.Histogram() {
+			if i == 0 || uint64(i) == Nbins {
+				// Don't expect max or min to match the given quantile for the distribution
+				continue
+			}
+			Q := cd.X
+			p := cd.P
+			if q.Quantile(p) != Q {
+				t.Errorf("For i: %d lambda: %v and p: %v Expected function Quantile %v, got %v", i, lambda, p, Q, q.Quantile(p))
+			}
+			if q.CDF(Q) != p {
+				t.Errorf("For i: %d lambda: %v and Q: %v Expected function CDF %v, got %v", i, lambda, Q, p, q.CDF(Q))
+			}
+			if math.Abs((exponentialQuantile(p, lambda)-Q)/exponentialQuantile(p, lambda)) > eps {
+				t.Errorf("For i: %d lambda: %v and p: %v Expected Histogram quantile %v, got %v", i, lambda, p, exponentialQuantile(p, lambda), Q)
+			}
+		}
+		min := q.Min()
+		max := q.Max()
+		// test the bounds
+		if q.Quantile(-1) != min {
+			t.Errorf("Expected percentage <0 to return min: %v got %v", min, q.Quantile(-1))
+		}
+		if q.Quantile(2) != max {
+			t.Errorf("Expected percentage >1 to return max: %v got %v", max, q.Quantile(2))
+		}
+		if q.CDF(min-1.0) != 0.0 {
+			t.Errorf("Expected CDF of x < min to return 0.0, got %v", q.CDF(min-1.0))
+		}
+		if q.CDF(max+1.0) != 1.0 {
+			t.Errorf("Expected  CDF of x > max to return 1.0, got %v", q.CDF(max+1.0))
+		}
+		for _, p := range ps {
+			if math.Abs((exponentialQuantile(p, lambda)-q.Quantile(p))/exponentialQuantile(p, lambda)) > eps {
+				t.Errorf("For lambda: %v and p: %v Expected Quantile %v, got %v", lambda, p, exponentialQuantile(p, lambda), q.Quantile(p))
+			}
 		}
 	}
 }
